75b16b69762c5a1e556e66d778fafb03e20672f2
bc2024-4 <bzabrodskij20@gmail.com>
2025-03-24 12:07:32 +0200

    add program

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,4 @@
+from django.views import generic
 from django.shortcuts import render
 
 from taxi.models import Driver, Car, Manufacturer
@@ -11,5 +12,34 @@ def index(request):
         "num_cars": Car.objects.count(),
         "num_manufacturers": Manufacturer.objects.count(),
     }
-
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(generic.ListView):
+    model = Manufacturer
+    paginate_by = 5
+    ordering = ['name']
+
+
+class CarListView(generic.ListView):
+    model = Car
+    paginate_by = 5
+    queryset = Car.objects.select_related("manufacturer")
+
+
+class CarDetailView(generic.DetailView):
+    model = Car
+
+
+class DriverView(generic.ListView):
+    model = Driver
+    paginate_by = 5
+
+
+class DriverDetailView(generic.DetailView):
+    model = Driver
+    queryset = Driver.objects.prefetch_related(
+        "cars__manufacturer"
+    )
+    context_object_name = "driver"
+    template_name = "taxi/driver_detail.html"
